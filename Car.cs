1411e924a2e18cce854e40dc425c07389848781d
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-06-08 12:52:00 +0200

    added colors fo rthe car

--- Car.cs
+++ Car.cs
@@ -8,32 +8,127 @@ namespace cicli
 {
     internal class Car
     {
-        public int SerialNumber { get; set; }
+        public object FinalSerial { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public string EngineType { get; set; }
+        public CarEngine EngineType { get; set; }
         public string Displacement { get; set; }
-        
-      
+        public string Color { get; set; }
+
+        public enum CarEngine
+        {
+            Diesel = 1,
+            Gasoline,
+            Hybrid,
+            Electric
+            
+        }
         public void Carasaking()
         {
-            Console.WriteLine("name of the brand:");
-            Brand = Console.ReadLine();
-            Console.WriteLine("Car model's name:");
-            Model = Console.ReadLine();
-            Console.WriteLine("Enginetype:");
-            EngineType = Console.ReadLine();
-            Console.WriteLine("Displacement:");
-            Displacement =Console.ReadLine();
-            Console.WriteLine("Car successfully registered press ani key to retun to menu");
+            //asking Brand's name
+            do
+            {
+                Console.WriteLine("name of the brand:");
+                Brand = Console.ReadLine();
+                if (string.IsNullOrEmpty(Brand))
+                {
+                    Console.WriteLine("Brand can not be empty");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+
+            //asking Model's name
+            do
+            {
+
+                Console.WriteLine("Car model's name:");
+                Model = Console.ReadLine();
+                if (string.IsNullOrEmpty(Model))
+                {
+                    Console.WriteLine("Model can not be empty");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+            //asking Enginetype
+
+            do
+            {
+
+                Console.WriteLine("Enginetype: Diesel,Gasoline,Hybrid,Electric");
+                 string engineinput = Console.ReadLine();
+                if (Enum.TryParse(engineinput, true ,out CarEngine enginetype))
+                {
+                    EngineType = enginetype;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("fuel not in the list");
+                    continue;
+                }
+            } while (true);
+
+            //asking Displacement
+            do
+            {
+
+                Console.WriteLine("Displacement:");
+                Displacement = Console.ReadLine();
+                if (!Displacement.Contains('.'))
+                {
+                    Console.WriteLine("You forgot the dot");
+                    continue;
+                }
+                else if (string.IsNullOrEmpty(Model))
+                {
+                    Console.WriteLine("Model can not be empty");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (true);
+            //asking color
+            do
+            {
+                List<string> colors =new List<string>{ "Black", "black", "Pink", "pink", "Blue", "blue", "Purple", "purple", "Gray", "gray", "Red", "red", "Green", "green", "Yellow", "yellow" };
+                Console.WriteLine("What is the color of the car?");
+                Color = Console.ReadLine();
+
+                colors.FindAll(color => color.Contains(Color));
+                if(colors.Contains(Color))
+                {
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("colore non trovato");
+                }
+
+            } while (true);
+            Console.WriteLine("Car successfully registered press any key to return to menu");
             Console.ReadKey();
 
         }
 
-        
+
         public override string ToString()
         {
-            return $"SerialNumber: AA{SerialNumber},Brand:{Brand}, Manufacturer: {EngineType}, Model: {Model}, Displacement:{Displacement}cc";
+            Console.Clear();
+            return $"SerialNumber: {FinalSerial},Brand:{Brand}, Manufacturer: {(int)EngineType}, Model: {Model}, Displacement:{Displacement}cc, Color:{Color}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ List<Car> list = new List<Car>();
 for (int i = 0; i < 22; i++)
 {
 Car CarRegistry = new Car();  
+    Console.Clear();
     registro.welcome();
     Console.WriteLine("1.Register \n2.View\n3.Search\n4.Exit\n5.Car");
     Console.WriteLine("");
@@ -85,17 +86,27 @@ Car CarRegistry = new Car();
         }
         else if (input == 5)
         {
+            //Car section
+
             Console.Clear();
-            Console.WriteLine("what do you want to do here?");
+            Console.WriteLine("What do you want to do here?");
             Console.WriteLine("1.register a car\n2.show all car registered\n3.search a car");
             int InputCar = Convert.ToInt32(Console.ReadLine());
                 
             if (InputCar == 1)
             {
-
+                //creating a random and incremental serial number
+                Random rnd = new Random();
+                int Random = rnd.Next(0, 6);
+                string[] SerialLetter = { "A", "B", "C", "D","E","F","G" };
                 int repeat = 1;
-                CarRegistry.SerialNumber = i + repeat;
-                Console.WriteLine("AA" + CarRegistry.SerialNumber);
+                int SerialNumber = i + repeat;
+
+                CarRegistry.FinalSerial = SerialLetter[Random + 1]+ SerialLetter[Random] + SerialNumber;
+                Console.WriteLine(CarRegistry.FinalSerial);
+                //Console.WriteLine("AA" + CarRegistry.SerialNumber);
+
+                //program ask user the necessary informations about the car 
                 CarRegistry.Carasaking();
                 list.Add(CarRegistry);
                 Console.WriteLine("-----------------------------------");
@@ -103,13 +114,30 @@ Car CarRegistry = new Car();
 
             }else if(InputCar == 2)
             {
+                //section to show the all information inserted
+                Console.Clear();
+                Console.WriteLine("CARS LIST:");
+                Console.WriteLine("*****************************************");
+
                 foreach (Car item in list)
                 {
                     Console.WriteLine(item.ToString());
                 }
-            }else if(InputCar == 3)
+
+                if(list.Count == 0)
+                {
+                    Console.WriteLine("No Cars in list");
+                }
+                Console.WriteLine("press any key to return back");
+                Console.ReadKey();
+
+            }
+            else if(InputCar == 3)
             {
+                //search by brand's name section
                 Console.Clear();
+
+                //input to search 
                 Console.WriteLine("Enter the Brand's name to search:");
                 string searchCar = Console.ReadLine();
                 
@@ -117,7 +145,7 @@ Car CarRegistry = new Car();
                 List<Car>lista = new List<Car>();
                 
                 
-
+                //checking if the input contains any elements in the list
                 if (!string.IsNullOrEmpty(searchCar))
                 {
                     lista = list.FindAll(elem => elem.Brand.Contains(searchCar));
